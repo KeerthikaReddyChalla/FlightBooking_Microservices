37561daaae0eb0b985fff2fb56f58ac3ad203ef2
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-11-30 01:17:01 +0530

    add config for notification service

---
+++ Notification-service/src/main/java/com/flightapp/notifservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,29 @@
+package com.flightapp.notifservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "notification-consumers");
+        props.put("auto.offset.reset", "earliest");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
